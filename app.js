713ad6564de4c9bd44b4de9ed04bac9d06aca253
Nicola Egloff <negl@vzch.com>
2024-09-26 12:51:18 +0200

    Moved script into new file

---
+++ app.js
@@ -0,0 +1,22 @@
+const container = document.getElementById("Container");
+        const numberInput = document.getElementById("NumberInput");
+        const timeDisplay = document.getElementById("StopWatchDisplay");
+
+        function LoadDivs() {
+            let startTime = new Date();
+            let htmlContent = '';
+
+            for (let index = 1; index <= numberInput.value; index++) {
+                htmlContent += `<p class="TestContainer">
+                    Test ${index}
+                    </p>`;
+            }
+            
+            container.innerHTML = htmlContent;
+            timeDisplay.innerHTML = `Time: ${(new Date - startTime)}ms`;
+        }
+
+        function Clear(){
+            container.innerHTML = '';
+            timeDisplay.innerHTML = ''
+        }
