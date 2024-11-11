38644ccbe6468e1d344d105b00204f40d6a3a853
Nicola Egloff <negl@vzch.com>
2024-11-11 11:36:19 +0100

    Fixed container

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const container = document.getElementById("Container");
-        const numberInput = document.getElementById("NumberInput");
-        const timeDisplay = document.getElementById("StopWatchDisplay");
+const testItemContainer = document.getElementById("testItemContainer");
+const numberInput = document.getElementById("NumberInput");
+const timeDisplay = document.getElementById("StopWatchDisplay");
 
-        function LoadDivs() {
-            let startTime = new Date();
-            let htmlContent = '';
+function LoadDivs() {
+    let startTime = new Date();
+    let htmlContent = '';
 
-            for (let index = 1; index <= numberInput.value; index++) {
-                htmlContent += `<p class="TestContainer">
-                    Test ${index}
-                    </p>`;
-            }
-            
-            container.innerHTML = htmlContent;
-            timeDisplay.innerHTML = `Time: ${(new Date - startTime)}ms`;
-        }
+    for (let index = 1; index <= numberInput.value; index++) {
+        htmlContent += `<p class="TestContainer">
+            Test ${index}
+            </p>`;
+    }
+    
+    testItemContainer.innerHTML = htmlContent;
+    timeDisplay.innerHTML = `Time: ${(new Date - startTime)}ms`;
+}
 
-        function Clear(){
-            container.innerHTML = '';
-            timeDisplay.innerHTML = 'Time: XXXms'
-        }
+function Clear(){
+    testItemContainer.innerHTML = '';
+    timeDisplay.innerHTML = 'Time: XXXms'
+}
