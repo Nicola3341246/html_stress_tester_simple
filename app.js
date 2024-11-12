3d12fabf0f255c1f42529dd6c4c860959f510795
Nicola Egloff <negl@vzch.com>
2024-11-12 15:34:41 +0100

    Added dark/light-mode

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const testItemContainer = document.getElementById("testItemContainer");
 const numberInput = document.getElementById("NumberInput");
 const timeDisplay = document.getElementById("StopWatchDisplay");
+const currentTheme = localStorage.getItem('theme');
 var form = document.getElementById("inputRow");
+var toggleSwitch = document.getElementById("checkbox");
 
-function handleForm(event) { event.preventDefault(); } 
-form.addEventListener('submit', handleForm);
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
 
 function LoadDivs() {
     let startTime = new Date();
     let htmlContent = '';
-
+    
     for (let index = 1; index <= numberInput.value; index++) {
         htmlContent += `<p class="TestContainer">
-            Test ${index}
-            </p>`;
+        Test ${index}
+        </p>`;
     }
     
     testItemContainer.innerHTML = htmlContent;
@@ -24,3 +31,18 @@ function Clear(){
     testItemContainer.innerHTML = '';
     timeDisplay.innerHTML = 'Time: XXXms'
 }
+
+function SwitchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }    
+}
+
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+toggleSwitch.addEventListener('change', SwitchTheme, false);
