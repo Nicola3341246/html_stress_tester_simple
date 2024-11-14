08a0e580722862c514d049eb4a0ccd13773c3f3f
Igor Martic <IMar@vzch.com>
2024-11-14 17:40:46 +0100

    improved design and removed comments

--- pages/question/question.js
+++ pages/question/question.js
@@ -1,10 +1,9 @@
 class Question {
 
     moveNoButton() {
-        const container = document.querySelector('.container');
+        const container = document.querySelector('.Container');
         const noButton = document.getElementById('noButton');
         
-        // Generiere zufällige Positionen innerhalb des Containers
         const containerRect = container.getBoundingClientRect();
         const maxX = containerRect.width - noButton.offsetWidth;
         const maxY = containerRect.height - noButton.offsetHeight;
@@ -12,31 +11,25 @@ class Question {
         const randomX = Math.random() * maxX;
         const randomY = Math.random() * maxY;
     
-        // Setze die neue Position des Buttons
         noButton.style.position = 'absolute';
         noButton.style.left = `${randomX}px`;
         noButton.style.top = `${randomY}px`;
     }
     
     showCongratulations() {
-        const container = document.querySelector('.container');
+        const container = document.querySelector('.Container');
         
-        // Entferne den gesamten Inhalt des Containers
         container.innerHTML = '';
         
-        // Erstelle ein neues <h1>-Element mit dem Text "Gratulieren"
         const congratsMessage = document.createElement('h1');
         congratsMessage.textContent = 'Gratulieren';
         
-        // Füge die Nachricht dem leeren Container hinzu
         container.appendChild(congratsMessage);
     }
     
 }
 
-// Erstelle eine Instanz der Klasse Question
 const question = new Question();
 
-// Weise die Methoden der Instanz globale Referenzen zu, damit sie im HTML aufgerufen werden können
 window.moveNoButton = question.moveNoButton.bind(question);
 window.showCongratulations = question.showCongratulations.bind(question);
