97f21d895dffc357a23dd3a64b1ba8a47d0a1279
Nicola Egloff <negl@vzch.com>
2024-11-14 16:35:15 +0100

    Added hompage

--- pages/home/home.js
+++
@@ -1,7 +0,0 @@
-const snakeButton = document.getElementById("snakeButton");
-snakeButton.addEventListener("click", OpenSnakePage);
-
-function OpenSnakePage(){
-    console.log("opensnake");
-    window.location.replace = "http://127.0.0.1:5500/?#snake";
-}
